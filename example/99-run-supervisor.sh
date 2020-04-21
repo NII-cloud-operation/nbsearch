da2a965b9a42224cb38ed26eda6f5ef9f2154814
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-04-21 12:05:55 +0900

    Prevent to create index for nbsearch-tmp/

--- example/99-run-supervisor.sh
+++ example/99-run-supervisor.sh
@@ -9,7 +9,7 @@ if [[ ! -f /home/$NB_USER/.nbsearch/config_local.py ]] ; then
     while ! nc -z localhost 27017; do
       sleep 0.1 # wait for 1/10 of the second before check again
     done
-    jupyter nbsearch update-index /home/$NB_USER/.jupyter/jupyter_notebook_config.py local
+    jupyter nbsearch update-index /home/$NB_USER/.jupyter/jupyter_notebook_config.py --debug local
 fi
 
 export SUPERVISOR_INITIALIZED=1
